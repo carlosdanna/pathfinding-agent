6fd1273b4350c8b6602cd38b889256259c91f04c
Carlos Danna <CARLOS.DANNA@asurion.com>
2016-12-01 11:06:29 -0500

    initial commit

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+
+
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        //NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Goto(Transform goal) {
+        agent.destination = goal.position;
+    }
+}
